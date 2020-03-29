dfbbe122543b831042942a74059635b821ad4165
lukas1013 <leonardolukas10@gmail.com>
2020-03-29 00:48:19 -0300

    bugs fixed - missing name column and missing schema property of the knex object

--- backend/src/database/migrations/20200325190832_create_ongs.js
+++ backend/src/database/migrations/20200325190832_create_ongs.js
@@ -2,6 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('ongs', table =>{
   	table.string('id').primary();
+  	table.string('name').notNullable();
   	table.string('email').notNullable();
   	table.string('whatsapp').notNullable();
   	table.string('city').notNullable();
--- backend/src/database/migrations/20200325202602_create_incidents.js
+++ backend/src/database/migrations/20200325202602_create_incidents.js
@@ -5,14 +5,11 @@ exports.up = function(knex) {
   	table.string('title').notNullable();
   	table.string('description').notNullable();
   	table.string('value').notNullable();
-  	
   	table.string('ong_id').notNullable();
-  	
   	table.foreign('ong_id').references('id').inTable('ongs');
-  	
-  })
+  });
 };
 
 exports.down = function(knex) {
-  return knex.dropTable('incidents');
+  return knex.schema.dropTable('incidents');
 };
